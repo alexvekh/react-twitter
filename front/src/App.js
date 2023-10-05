4aed0a198d0d05d51b5eaed6de0a677dd5d43c0f
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-10-04 20:10:04 -0400

    memo, lezy, Suspemse

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import { lazy, useEffect, useState, Suspense } from "react";
 import Page from "./component/page";
-import PostList from "./container/post-list";
+
+const Child = lazy(() => import("./Child"));
 
 function App() {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const id = setInterval(() => setValue((prev) => prev + 1), 1000);
+    return () => {
+      clearInterval(id);
+    };
+  }, []);
+
   return (
     <Page>
-      <PostList />
+      <div>state: {value}</div>
+      <div>
+        {value > 5 && (
+          <Suspense fallback={<div>Loading ... </div>}>
+            <Child value={value} />
+          </Suspense>
+        )}
+      </div>
     </Page>
   );
 }
